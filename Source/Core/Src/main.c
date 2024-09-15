0ae2c33ed27de092838457d2a1c48a9a386781c3
ZICandnothingmore <baonhu0508@gmail.com>
2024-09-15 23:24:28 +0700

    UPDATE EX3

--- Source/Core/Src/main.c
+++ Source/Core/Src/main.c
@@ -23,6 +23,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "software_timer.h"
+#include "Ex3.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -93,6 +94,7 @@ int main(void)
   HAL_GPIO_WritePin(GPIOB, a_Pin|b_Pin|c_Pin|d_Pin|e_Pin|f_Pin
                           |g_Pin, GPIO_PIN_SET);
   HAL_GPIO_WritePin(GPIOA, EN0_Pin|EN1_Pin|EN2_Pin|EN3_Pin, 1);
+  ex3_init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -107,6 +109,7 @@ int main(void)
 		setTimer1(100);
 		//TODO
 		HAL_GPIO_TogglePin(RED_LED_GPIO_Port, RED_LED_Pin);
+		ex3_run();
 	}
   }
   /* USER CODE END 3 */
