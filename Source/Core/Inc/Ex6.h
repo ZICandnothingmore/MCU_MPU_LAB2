05bbe7fb0fd70f6dad42798504d4e4ce7141db58
ZICandnothingmore <baonhu0508@gmail.com>
2024-09-19 15:31:50 +0700

    update main.c

---
+++ Source/Core/Inc/Ex6.h
@@ -0,0 +1,13 @@
+/*
+ * Ex6.h
+ *
+ *  Created on: Sep 19, 2024
+ *      Author: ACER
+ */
+
+#ifndef INC_EX6_H_
+#define INC_EX6_H_
+
+#include "main.h"
+
+#endif /* INC_EX6_H_ */
---
+++ Source/Core/Src/Ex6.c
@@ -0,0 +1,10 @@
+/*
+ * Ex6.c
+ *
+ *  Created on: Sep 19, 2024
+ *      Author: ACER
+ */
+
+#include "Ex6.h"
+
+
