dfe0f88379bfe5638f7a8f288bda1ab505f08e73
ZICandnothingmore <baonhu0508@gmail.com>
2024-10-08 17:08:16 +0700

    update
    done

--- Source/Core/Src/main.c
+++ Source/Core/Src/main.c
@@ -22,7 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "software_timer.h"
-#include "Ex8.h"
+//#include "Ex8.h"
 
 /* USER CODE END Includes */
 
@@ -57,7 +57,184 @@ static void MX_TIM2_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+int hour, minute, second;
+//int hour_digit1, hour_digit2, min_digit1, min_digit2;
+const int MAX_LED = 4;
+int index_led = 0;
+int led_buffer[] = {1,2,3,4};
 
+void display7SEG(int num){
+	if (num == 0){
+		HAL_GPIO_WritePin(a_GPIO_Port, a_Pin, 0);
+		HAL_GPIO_WritePin(b_GPIO_Port, b_Pin, 0);
+		HAL_GPIO_WritePin(c_GPIO_Port, c_Pin, 0);
+		HAL_GPIO_WritePin(d_GPIO_Port, d_Pin, 0);
+		HAL_GPIO_WritePin(e_GPIO_Port, e_Pin, 0);
+		HAL_GPIO_WritePin(f_GPIO_Port, f_Pin, 0);
+		HAL_GPIO_WritePin(g_GPIO_Port, g_Pin, 1);
+	}
+	if (num == 1){
+		HAL_GPIO_WritePin(a_GPIO_Port, a_Pin, 1);
+		HAL_GPIO_WritePin(b_GPIO_Port, b_Pin, 0);
+		HAL_GPIO_WritePin(c_GPIO_Port, c_Pin, 0);
+		HAL_GPIO_WritePin(d_GPIO_Port, d_Pin, 1);
+		HAL_GPIO_WritePin(e_GPIO_Port, e_Pin, 1);
+		HAL_GPIO_WritePin(f_GPIO_Port, f_Pin, 1);
+		HAL_GPIO_WritePin(g_GPIO_Port, g_Pin, 1);
+	}
+	if (num == 2){
+		HAL_GPIO_WritePin(a_GPIO_Port, a_Pin, 0);
+		HAL_GPIO_WritePin(b_GPIO_Port, b_Pin, 0);
+		HAL_GPIO_WritePin(c_GPIO_Port, c_Pin, 1);
+		HAL_GPIO_WritePin(d_GPIO_Port, d_Pin, 0);
+		HAL_GPIO_WritePin(e_GPIO_Port, e_Pin, 0);
+		HAL_GPIO_WritePin(f_GPIO_Port, f_Pin, 1);
+		HAL_GPIO_WritePin(g_GPIO_Port, g_Pin, 0);
+	}
+	if (num == 3){
+		HAL_GPIO_WritePin(a_GPIO_Port, a_Pin, 0);
+		HAL_GPIO_WritePin(b_GPIO_Port, b_Pin, 0);
+		HAL_GPIO_WritePin(c_GPIO_Port, c_Pin, 0);
+		HAL_GPIO_WritePin(d_GPIO_Port, d_Pin, 0);
+		HAL_GPIO_WritePin(e_GPIO_Port, e_Pin, 1);
+		HAL_GPIO_WritePin(f_GPIO_Port, f_Pin, 1);
+		HAL_GPIO_WritePin(g_GPIO_Port, g_Pin, 0);
+	}
+	if (num == 4){
+		HAL_GPIO_WritePin(a_GPIO_Port, a_Pin, 1);
+		HAL_GPIO_WritePin(b_GPIO_Port, b_Pin, 0);
+		HAL_GPIO_WritePin(c_GPIO_Port, c_Pin, 0);
+		HAL_GPIO_WritePin(d_GPIO_Port, d_Pin, 1);
+		HAL_GPIO_WritePin(e_GPIO_Port, e_Pin, 1);
+		HAL_GPIO_WritePin(f_GPIO_Port, f_Pin, 0);
+		HAL_GPIO_WritePin(g_GPIO_Port, g_Pin, 0);
+	}
+	if (num == 5){
+		HAL_GPIO_WritePin(a_GPIO_Port, a_Pin, 0);
+		HAL_GPIO_WritePin(b_GPIO_Port, b_Pin, 1);
+		HAL_GPIO_WritePin(c_GPIO_Port, c_Pin, 0);
+		HAL_GPIO_WritePin(d_GPIO_Port, d_Pin, 0);
+		HAL_GPIO_WritePin(e_GPIO_Port, e_Pin, 1);
+		HAL_GPIO_WritePin(f_GPIO_Port, f_Pin, 0);
+		HAL_GPIO_WritePin(g_GPIO_Port, g_Pin, 0);
+	}
+	if (num == 6){
+		HAL_GPIO_WritePin(a_GPIO_Port, a_Pin, 0);
+		HAL_GPIO_WritePin(b_GPIO_Port, b_Pin, 1);
+		HAL_GPIO_WritePin(c_GPIO_Port, c_Pin, 0);
+		HAL_GPIO_WritePin(d_GPIO_Port, d_Pin, 0);
+		HAL_GPIO_WritePin(e_GPIO_Port, e_Pin, 0);
+		HAL_GPIO_WritePin(f_GPIO_Port, f_Pin, 0);
+		HAL_GPIO_WritePin(g_GPIO_Port, g_Pin, 0);
+	}
+	if (num == 7){
+		HAL_GPIO_WritePin(a_GPIO_Port, a_Pin, 0);
+		HAL_GPIO_WritePin(b_GPIO_Port, b_Pin, 0);
+		HAL_GPIO_WritePin(c_GPIO_Port, c_Pin, 0);
+		HAL_GPIO_WritePin(d_GPIO_Port, d_Pin, 1);
+		HAL_GPIO_WritePin(e_GPIO_Port, e_Pin, 1);
+		HAL_GPIO_WritePin(f_GPIO_Port, f_Pin, 1);
+		HAL_GPIO_WritePin(g_GPIO_Port, g_Pin, 1);
+	}
+	if (num == 8){
+		HAL_GPIO_WritePin(a_GPIO_Port, a_Pin, 0);
+		HAL_GPIO_WritePin(b_GPIO_Port, b_Pin, 0);
+		HAL_GPIO_WritePin(c_GPIO_Port, c_Pin, 0);
+		HAL_GPIO_WritePin(d_GPIO_Port, d_Pin, 0);
+		HAL_GPIO_WritePin(e_GPIO_Port, e_Pin, 0);
+		HAL_GPIO_WritePin(f_GPIO_Port, f_Pin, 0);
+		HAL_GPIO_WritePin(g_GPIO_Port, g_Pin, 0);
+	}
+	if (num == 9){
+		HAL_GPIO_WritePin(a_GPIO_Port, a_Pin, 0);
+		HAL_GPIO_WritePin(b_GPIO_Port, b_Pin, 0);
+		HAL_GPIO_WritePin(c_GPIO_Port, c_Pin, 0);
+		HAL_GPIO_WritePin(d_GPIO_Port, d_Pin, 0);
+		HAL_GPIO_WritePin(e_GPIO_Port, e_Pin, 1);
+		HAL_GPIO_WritePin(f_GPIO_Port, f_Pin, 0);
+		HAL_GPIO_WritePin(g_GPIO_Port, g_Pin, 0);
+	}
+}
+
+void update7SEG(int index){
+	switch(index){
+		case 0:
+			//TODO
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+			display7SEG(led_buffer[0]);
+			break;
+		case 1:
+			//TODO
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+			display7SEG(led_buffer[1]);
+			break;
+		case 2:
+			//TODO
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 0);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+			display7SEG(led_buffer[2]);
+			break;
+		case 3:
+			//TODO
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 0);
+			display7SEG(led_buffer[3]);
+			break;
+		default:
+			break;
+	}
+}
+
+
+void updateClockBuffer(){
+	led_buffer[0] = hour / 10;
+	led_buffer[1] = hour % 10;
+	led_buffer[2] = minute / 10;
+	led_buffer[3] = minute % 10;
+}
+
+void clock_run(){
+	second++;
+	if (second >= 60){
+		second = 0;
+		minute++;
+	}
+	if (minute >= 60){
+		minute = 0;
+		hour++;
+	}
+	if (hour >= 24){
+		hour = 0;
+	}
+	updateClockBuffer();
+}
+
+void ex8_init(){
+	hour = 15;
+	minute = 8;
+	second = 50;
+	HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
+	HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+	HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+	HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+	updateClockBuffer();
+}
+
+void ex8_run(){
+	//clock_run();
+	if (index_led > 3) index_led = 0;
+	update7SEG(index_led++);
+}
 /* USER CODE END 0 */
 
 /**
@@ -111,12 +288,13 @@ int main(void)
 		//TODO
 		HAL_GPIO_TogglePin(RED_LED_GPIO_Port, RED_LED_Pin);
 		HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+		clock_run();
 	}
 	if (isTimerExpired(1) == 1){
 		setTimer(1, 25);
 		//TODO
 		ex8_run();
-		}
+	}
   }
   /* USER CODE END 3 */
 }
